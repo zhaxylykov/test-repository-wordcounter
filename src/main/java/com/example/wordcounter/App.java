57a26b63a8954111125c5d02b6cc57c076805895
zhaxylykov <dias.zhaxylykov@nu.edu.kz>
2018-01-29 13:34:59 +0600

    Version 1.0

--- src/main/java/com/example/wordcounter/App.java
+++ src/main/java/com/example/wordcounter/App.java
@@ -4,10 +4,25 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Word Count Testing App!");
+        System.out.println(wordcount("Word Count Testing App!"));
     }
     
     public static int wordcount(String s) {
-        return 0;
+        int count = 0;
+	    	String s = "";
+	    	char a[] = s.toCharArray();
+	    	int endLine = s.length() -1;
+	    	for (int i = 0; i<s.length(); i++) {
+	    		if (a[i] == 0) {
+	    			break;
+	    		}
+	    		else if (a[i] == ' ' || i == endLine) {
+	    			count++;
+	    		} else {
+	    			continue;
+	    		}
+	    	}
+	    	assertEquals(0, count, 0);
+		return count;
     }
 }
